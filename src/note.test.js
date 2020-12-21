09ae79ac4ca3b36ef87c9301c2c9f817a1fdde9a
Matthew Lock <matthew_lock20@hotmail.com>
2020-12-21 18:01:14 +0000

    Second test written for testing id

--- src/note.test.js
+++ src/note.test.js
@@ -5,4 +5,9 @@ describe ('#Note', () => {
     let note = new Note("Buy the milk")
     expect(note.todo).toBe("Buy the milk")
   })
+
+  it("Allows note to have an id", () => {
+    let note = new Note("Purchase a tote", 1)
+    expect(note.id).toBe(1)
+  })
 })
