6d2749417989dd519fc761e1758afffc08933001
Matthew Lock <matthew_lock20@hotmail.com>
2020-12-22 14:53:47 +0000

    Additional test written and passed for 1 note

--- spec/noteList.test.js
+++ spec/noteList.test.js
@@ -3,7 +3,12 @@ import NoteList from "../src/noteList"
 describe("#NoteList", () => {
   it("Displays empty list if no notes are inside", () => {
     let noteList = new NoteList()
-
     expect(noteList.noteArray).toStrictEqual([])
+  })
+  
+  it("Displays list of 1 note when user adds first item to list", () => {
+    let noteList = new NoteList()
+    noteList.add("Buy bread")
+    expect(noteList.noteArray.length).toEqual(1)
   })
 })
--- src/noteList.js
+++ src/noteList.js
@@ -2,4 +2,8 @@ export default class NoteList {
   constructor(noteArray) {
     this.noteArray = [];
   }
+
+  add(note) {
+    this.noteArray.push(note)
+  }
 }
