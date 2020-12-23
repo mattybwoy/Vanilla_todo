e49e17066de940a80b87b60b76210453c92cf2d6
Matthew Lock <matthew_lock20@hotmail.com>
2020-12-23 15:06:57 +0000

    Full note now displays under heading

--- interface.js
+++ interface.js
@@ -6,6 +6,8 @@ let noteList = new NoteList();
 let btn = document.getElementById("createNote");
 btn.addEventListener("click", add);
 
+window.addEventListener("hashchange", displayFull);
+
 function add() {
     noteList.add(document.getElementById("user_input").value);
     document.getElementById("user_input").value = "";
@@ -13,13 +15,17 @@ function add() {
   }
 
   function displayNote() {
-    var notesList = document.getElementById("notesList");
-    var li = document.createElement("a");
-    var br = document.createElement("br");
+    let notesList = document.getElementById("notesList");
+    let li = document.createElement("a");
+    let br = document.createElement("br");
     li.setAttribute('id',(noteList.noteArray.length -1));
     li.setAttribute('class', 'noteyList');
-    li.setAttribute('href',('#'+(noteList.sliceArray.length -1)));
+    li.setAttribute('href',('#'+(noteList.noteArray.length -1)));
     li.appendChild(document.createTextNode(noteList.sliceArray[noteList.sliceArray.length -1]));
     notesList.appendChild(li);
     notesList.appendChild(br);
+  }
+
+  function displayFull() {
+    document.getElementById("displayNote").innerHTML = noteList.noteArray[location.hash.substr(1)]
   }
